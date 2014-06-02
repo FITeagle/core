7407ed3ba8a63d94db10254dae92722c32912d9b
Mitja <mitjanikolaus@gmail.com>
2014-06-02 17:26:13 +0200

    forgot to commit new files

---
+++ usermanagement/src/main/java/org/fiteagle/core/usermanagement/dm/UserManagerEJB.java
@@ -0,0 +1,184 @@
+package org.fiteagle.core.usermanagement.dm;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.fiteagle.api.core.usermanagement.Class;
+import org.fiteagle.api.core.usermanagement.User;
+import org.fiteagle.api.core.usermanagement.User.PublicKeyNotFoundException;
+import org.fiteagle.api.core.usermanagement.User.Role;
+import org.fiteagle.api.core.usermanagement.UserManager;
+import org.fiteagle.api.core.usermanagement.UserPublicKey;
+import org.fiteagle.core.aaa.authentication.PasswordUtil;
+
+@Stateless
+@Remote(UserManager.class)
+public class UserManagerEJB implements UserManager{
+
+	private final UserManager usermanager;
+	
+	public UserManagerEJB() throws NamingException{
+	  Context context;
+    context = new InitialContext();
+    usermanager = (UserManager) context.lookup("java:global/usermanagement/JPAUserManager");
+    if(!databaseContainsAdminUser()){
+      createFirstAdminUser();
+    }
+  }
+  
+  private void createFirstAdminUser() {
+//    log.info("Creating First Admin User");
+    String[] passwordHashAndSalt = PasswordUtil.generatePasswordHashAndSalt("admin");
+    User admin = User.createAdminUser("admin", passwordHashAndSalt[0], passwordHashAndSalt[1]);
+    usermanager.add(admin);
+  }
+  
+  private boolean databaseContainsAdminUser() {
+    List<User> users = usermanager.getAllUsers();
+    for (User u : users) {
+      if (u.getRole().equals(Role.FEDERATION_ADMIN)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+	@Override
+	public void add(User user) {
+		usermanager.add(user);
+	}
+
+	@Override
+	public User get(User user) {
+		return usermanager.get(user);
+	}
+
+	@Override
+	public User get(String username) {
+		return usermanager.get(username);
+	}
+
+	@Override
+	public void delete(User user) {
+		usermanager.delete(user);		
+	}
+
+	@Override
+	public void delete(String username) {
+		usermanager.delete(username);
+	}
+
+	@Override
+	public void update(String username, String firstName, String lastName,
+			String email, String affiliation, String password,
+			List<UserPublicKey> publicKeys) {
+		usermanager.update(username, firstName, lastName, email, affiliation, password, publicKeys);		
+	}
+
+	@Override
+	public void setRole(String username, Role role) {
+		usermanager.setRole(username, role);		
+	}
+
+	@Override
+	public void addKey(String username, UserPublicKey publicKey) {
+		usermanager.addKey(username, publicKey);
+	}
+
+	@Override
+	public void deleteKey(String username, String description) {
+		usermanager.deleteKey(username, description);		
+	}
+
+	@Override
+	public void renameKey(String username, String description,
+			String newDescription) {
+		usermanager.renameKey(username, description, newDescription);
+	}
+
+	@Override
+	public String createUserKeyPairAndCertificate(String username,
+			String passphrase) throws Exception {
+		return usermanager.createUserKeyPairAndCertificate(username, passphrase);
+	}
+
+	@Override
+	public String createUserCertificateForPublicKey(String username,
+			String description) throws Exception, PublicKeyNotFoundException {
+		return usermanager.createUserCertificateForPublicKey(username, description);
+	}
+
+	@Override
+	public boolean verifyCredentials(String username, String password)
+			throws NoSuchAlgorithmException, IOException, UserNotFoundException {
+		return usermanager.verifyCredentials(username, password);
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		return usermanager.getAllUsers();
+	}
+
+	@Override
+	public Class addClass(String ownerUsername, Class targetClass) {
+		return usermanager.addClass(ownerUsername, targetClass);
+	}
+
+	@Override
+	public Class get(Class targetClass) {
+		return usermanager.get(targetClass);
+	}
+
+	@Override
+	public Class get(long id) {
+		return usermanager.get(id);
+	}
+
+	@Override
+	public void delete(Class targetClass) {
+		usermanager.delete(targetClass);
+	}
+
+	@Override
+	public void delete(long id) {
+		usermanager.delete(id);
+	}
+
+	@Override
+	public void addParticipant(long id, String username) {
+		usermanager.addParticipant(id, username);
+	}
+
+	@Override
+	public void removeParticipant(long id, String username) {
+		usermanager.removeParticipant(id, username);
+	}
+
+	@Override
+	public List<Class> getAllClassesFromUser(String username) {
+		return usermanager.getAllClassesFromUser(username);
+	}
+
+	@Override
+	public List<Class> getAllClassesOwnedByUser(String username) {
+		return usermanager.getAllClassesOwnedByUser(username);
+	}
+
+	@Override
+	public List<Class> getAllClasses() {
+		return usermanager.getAllClasses();
+	}
+
+	@Override
+	public void deleteAllEntries() {
+		usermanager.deleteAllEntries();
+	}
+
+}
---
+++ usermanagement/src/main/java/org/fiteagle/core/usermanagement/dm/UserManagerMDB.java
@@ -0,0 +1,104 @@
+package org.fiteagle.core.usermanagement.dm;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Topic;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.fiteagle.api.core.IMessageBus;
+import org.fiteagle.api.core.usermanagement.User;
+import org.fiteagle.api.core.usermanagement.UserManager;
+import org.fiteagle.api.core.usermanagement.UserPublicKey;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+@MessageDriven(name="UserManagerMDB", activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+    @ActivationConfigProperty(propertyName = "destination", propertyValue = IMessageBus.TOPIC_CORE),
+    @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = UserManager.MESSAGE_FILTER),
+    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+public class UserManagerMDB implements MessageListener {
+
+  private final UserManager usermanager;
+  
+  @Inject
+  private JMSContext context;
+  @Resource(mappedName = IMessageBus.TOPIC_CORE_NAME)
+  private Topic topic;
+  
+  private final static Logger logger = Logger.getLogger(UserManagerMDB.class.toString());
+  
+  public UserManagerMDB() throws NamingException{
+    Context context;
+    context = new InitialContext();
+    usermanager = (UserManager) context.lookup("java:global/usermanagement/JPAUserManager");
+  }
+  
+  @Override
+  public void onMessage(final Message rcvMessage) {
+    try {
+      String methodName = rcvMessage.getStringProperty(IMessageBus.TYPE_REQUEST);
+      logger.info("Received a message: "+methodName);
+      Method method = null;
+      try {
+        method = usermanager.getClass().getMethod(methodName);
+      } catch (SecurityException e) {
+      } catch (NoSuchMethodException e) {        
+      }
+      List<User> users = null;
+      try{
+        users = (List<User>) method.invoke(usermanager);
+      } catch (IllegalArgumentException e) {
+      } catch (IllegalAccessException e) {
+      } catch (InvocationTargetException e) {
+      }
+
+      final String id = rcvMessage.getJMSCorrelationID();
+      
+      Gson gson = new GsonBuilder()
+      .setExclusionStrategies(new ExclusionStrategy() {
+          public boolean shouldSkipClass(Class<?> classToSkip) {
+             return false;
+          }
+          public boolean shouldSkipField(FieldAttributes f) {
+            return ((f.getDeclaringClass() == UserPublicKey.class && (f.getName().equals("owner") || f.getName().equals("publicKey"))) ||
+                (f.getDeclaringClass() == User.class && (f.getName().equals("classes") || f.getName().equals("classesOwned"))));
+          }
+       }) 
+      .create();
+      
+      final String resultJSON = gson.toJson(users);
+      
+      final Message message = this.context.createMessage();
+      message.setStringProperty(IMessageBus.TYPE_RESPONSE, UserManager.GET_ALL_USERS);
+      message.setStringProperty(IMessageBus.TYPE_RESULT, resultJSON);
+      if(id != null){
+        message.setJMSCorrelationID(id);
+      }
+      this.context.createProducer().send(topic, message);
+    } catch (final JMSException e) {      
+        logger.log(Level.SEVERE, "Issue with JMS", e);
+    }
+  }
+
+
+
+}
