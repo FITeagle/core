e99423335b7f341ee3b168599bae69a76c0f7d63
AlexanderWillner <alexander.willner@tu-berlin.de>
2014-06-18 15:39:41 +0200

    adding functionality to REST API

--- bus/src/main/java/org/fiteagle/core/bus/dm/MessageBusCommanderREST.java
+++ bus/src/main/java/org/fiteagle/core/bus/dm/MessageBusCommanderREST.java
@@ -1,27 +1,50 @@
 package org.fiteagle.core.bus.dm;
 
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.inject.Inject;
+import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.naming.NamingException;
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import org.fiteagle.api.core.IMessageBus;
 
 @Path("/")
 public class MessageBusCommanderREST {
 
 	private static Logger LOGGER = Logger
 			.getLogger(MessageBusCommanderREST.class.toString());
-	
-	public MessageBusCommanderREST() throws NamingException {
+	private MessageBusBean senderBean;
+
+	@Inject
+	public MessageBusCommanderREST(MessageBusBean senderBean)
+			throws NamingException {
 		LOGGER.log(Level.INFO, "Started REST");
+		this.senderBean = senderBean;
+	}
+	
+	public MessageBusCommanderREST() {
+		LOGGER.log(Level.INFO, "Started REST");
+		this.senderBean = new MessageBusBean();
 	}
 
-	@GET
+	@PUT
 	@Path("/commander")
-	public String commander() {
-		LOGGER.log(Level.INFO, "Commander REST API call...");
-		return "hello";
-	}
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String commander(String command) throws JMSException {
+		LOGGER.log(Level.INFO, "Received REST message: " + command);
 
+		final Message message = senderBean.createMessage();
+		message.setStringProperty(IMessageBus.TYPE_REQUEST, command);
+		message.setJMSCorrelationID(UUID.randomUUID().toString());
+
+		senderBean.sendMessage(message);
+		return "done sending '" + command + "'";
+	}
 }
--- bus/src/main/java/org/fiteagle/core/bus/dm/MessageBusCommanderWS.java
+++ bus/src/main/java/org/fiteagle/core/bus/dm/MessageBusCommanderWS.java
@@ -1,6 +1,7 @@
 package org.fiteagle.core.bus.dm;
 
 import java.io.IOException;
+
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
