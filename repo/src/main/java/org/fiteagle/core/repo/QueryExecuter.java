00a964a79c306a3605b969b7af36b04ecd9d67df
mitjanikolaus <mitjanikolaus@gmail.com>
2014-10-10 19:27:56 +0200

    refactoring

--- repo/src/main/java/org/fiteagle/core/repo/QueryExecuter.java
+++ repo/src/main/java/org/fiteagle/core/repo/QueryExecuter.java
@@ -1,46 +1,42 @@
 package org.fiteagle.core.repo;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.ResultSet;
 
 public class QueryExecuter {
-
-    private static final String FUSEKI_SERVICE = "http://localhost:3030/ds/query";
-
-    private static Logger LOGGER = Logger.getLogger(QueryExecuter.class.toString());
-
-    /**
-     * Submits a given sparql query string to the db
-     * @param sparqlQuery String containing the sparql query to be issued
-     * @return ResultSet retrieved from db, null on failure
-     */
-    public static ResultSet queryModelFromDatabase(String sparqlQuery){
-
-        QueryExecuter.LOGGER.log(Level.INFO, "Querying Fuseki Service");
-
-        ResultSet queryResult = submitSparqlQuery(sparqlQuery);
-
-        return queryResult;        
-    }
-    
-    /**
-     * Submits a given sparql query string to the db
-     * @param queryString String containing the sparql query to be issued
-     * @return ResultSet retrieved from db, null on failure
-     */
-    private static ResultSet submitSparqlQuery(String queryString){
-        ResultSet rs = null;
-    	try {
-            QueryExecution qe = QueryExecutionFactory.sparqlService(FUSEKI_SERVICE, queryString);
-            LOGGER.log(Level.INFO, "Now Sending query: "+ qe.getQuery().toString());
-            rs = qe.execSelect();
- 		} catch (Exception e){
- 			e.printStackTrace();
- 		}
-		return rs;
-	}
-    
-
+  
+  private static final String FUSEKI_SERVICE = "http://localhost:3030/ds/query";
+  
+  @SuppressWarnings("unused")
+  private static Logger LOGGER = Logger.getLogger(QueryExecuter.class.toString());
+  
+  /**
+   * Submits a given sparql query string to the db
+   * 
+   * @param sparqlQuery
+   *          String containing the sparql query to be issued
+   * @return ResultSet retrieved from db, null on failure
+   */
+  public static ResultSet queryModelFromDatabase(String sparqlQuery) {
+    ResultSet queryResult = submitSparqlQuery(sparqlQuery);
+    return queryResult;
+  }
+  
+  /**
+   * Submits a given sparql query string to the db
+   * 
+   * @param queryString
+   *          String containing the sparql query to be issued
+   * @return ResultSet retrieved from db, null on failure
+   */
+  private static ResultSet submitSparqlQuery(String queryString) {
+    ResultSet rs = null;
+    QueryExecution qe = QueryExecutionFactory.sparqlService(FUSEKI_SERVICE, queryString);
+    rs = qe.execSelect();
+    return rs;
+  }
+  
 }
--- repo/src/test/java/org/fiteagle/core/repo/QueryExecuterTest.java
+++ repo/src/test/java/org/fiteagle/core/repo/QueryExecuterTest.java
@@ -5,12 +5,9 @@ import org.junit.Test;
 
 public class QueryExecuterTest {
 
-	/**
-	 * Test Querying SPARQL from the endpoint
-	 */
 	@Test
 	public void testInvalidQuery(){
-		// Submitting invalid SPARQL Query should return null, whether Triplet store is running or not
 		Assert.assertNull(QueryExecuter.queryModelFromDatabase("This is no valid SPARQL Query"));
 	}
+	
 }
