d7d042098c3880377d3447da74242aceae8dd4e8
Alaa Alloush <alaa.alloush@gmail.com>
2014-10-21 13:03:58 +0200

    handling CONSTRUCT

--- repo/src/main/java/org/fiteagle/core/repo/QueryExecuter.java
+++ repo/src/main/java/org/fiteagle/core/repo/QueryExecuter.java
@@ -19,6 +19,19 @@ public class QueryExecuter {
   @SuppressWarnings("unused")
   private static Logger LOGGER = Logger.getLogger(QueryExecuter.class.toString());
   
+  private static Map<String, String> missedNsPrefixes = new HashMap<>();
+  
+  static{
+	  missedNsPrefixes.put("wgs", "http://www.w3.org/2003/01/geo/wgs84_pos#");
+	  missedNsPrefixes.put("motor", "http://fiteagle.org/ontology/adapter/motor#");
+	  missedNsPrefixes.put("omn","http://fiteagle.org/ontology#");
+	  missedNsPrefixes.put("mightyrobot","http://fiteagle.org/ontology/adapter/mightyrobot#");
+	  missedNsPrefixes.put("foaf","http://xmlns.com/foaf/0.1/");
+	  missedNsPrefixes.put("dc","http://purl.org/dc/elements/1.1/");
+	  missedNsPrefixes.put("","http://fiteagleinternal#"); 
+  }
+  
+  
   public static ResultSet executeSparqlSelectQuery(String queryString) throws QueryParseException{
     ResultSet rs = null;
     QueryExecution qe = QueryExecutionFactory.sparqlService(FUSEKI_SERVICE, queryString);
@@ -26,28 +39,32 @@ public class QueryExecuter {
     return rs;
   }
   
+  public static Model executeSparqlConstructQuery(String queryString) throws QueryParseException{
+	    Model rs = null;
+	    QueryExecution qe = QueryExecutionFactory.sparqlService(FUSEKI_SERVICE, queryString);
+	    rs = qe.execConstruct();
+	    
+	    //todo: find a better way to set our own common known prefixes (e.g. omn, wgs, ...)
+	    Map<String, String> nsPrefix = new HashMap<>();
+	    nsPrefix = rs.getNsPrefixMap();
+	    
+	    for(Map.Entry<String, String> entry : nsPrefix.entrySet()){
+	    	if (entry.getKey().toString().contains("j.")){
+	    		for(Map.Entry<String, String> staticEntry : missedNsPrefixes.entrySet()){
+	    			if(entry.getValue().toString().equals(staticEntry.getValue().toString())){
+	    				rs.setNsPrefix(staticEntry.getKey().toString(), staticEntry.getValue().toString());
+	    				rs.removeNsPrefix(entry.getKey());
+	    			}
+	    		}
+	    	}
+	    }
+	    return rs;
+	  }
+  
   public static Model executeSparqlDescribeQuery(String queryString) throws QueryParseException{
     Model rs = null;
     QueryExecution qe = QueryExecutionFactory.sparqlService(FUSEKI_SERVICE, queryString);
     rs = qe.execDescribe();
-    
-    //todo: find a better way to set our own common known prefixes (e.g. omn, wgs, ...)
-    Map<String, String> nsPrefix = new HashMap<>();
-    nsPrefix = rs.getNsPrefixMap();
-    for(Map.Entry<String, String> entry : nsPrefix.entrySet()){
-    	if (entry.getKey().toString().contains("j.")){
-    		rs.removeNsPrefix(entry.getKey());
-    	}
-    }
-//    rs.removeNsPrefix("j.0");
-//    rs.removeNsPrefix("j.1");
-//    rs.removeNsPrefix("j.2");
-    rs.setNsPrefix("omn", "http://fiteagle.org/ontology#");
-    rs.setNsPrefix("wgs", "http://www.w3.org/2003/01/geo/wgs84_pos#");
-    rs.setNsPrefix("foaf", "http://xmlns.com/foaf/0.1/");
-    rs.setNsPrefix("mightyrobot", "http://fiteagle.org/ontology/adapter/mightyrobot#");
-
-    //MessageBusMsgFactory.setCommonPrefixes(rs);
     return rs;
   }
   
