f8c3755047331a288e9f3eddc5ec192dce0dcab5
Alaa Alloush <alaa.alloush@gmail.com>
2015-01-02 15:18:02 +0100

    reserving resources via reservation module

--- reservation/src/main/java/org/fiteagle/core/reservation/dm/ReservationMDBListener.java
+++ reservation/src/main/java/org/fiteagle/core/reservation/dm/ReservationMDBListener.java
@@ -1,6 +1,8 @@
 package org.fiteagle.core.reservation.dm;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,13 +20,18 @@ import org.fiteagle.api.core.IMessageBus;
 import org.fiteagle.api.core.MessageBusOntologyModel;
 import org.fiteagle.api.core.MessageUtil;
 import org.fiteagle.core.tripletStoreAccessor.QueryExecuter;
+import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
 
+import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFList;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.RDF;
 
 @MessageDriven(name = "ReservationMDBListener", activationConfig = {
     @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
@@ -34,6 +41,8 @@ public class ReservationMDBListener implements MessageListener {
   
   private static Logger LOGGER = Logger.getLogger(ReservationMDBListener.class.toString());
   private static String componentManagerId = "component_manager_id";
+  private static String URN = "urn";
+  private static String EndTime = "endTime";
   
   @Inject
   private JMSContext context;
@@ -67,22 +76,33 @@ public class ReservationMDBListener implements MessageListener {
   private void handleRequest(Model requestModel, Resource messageResource, String serialization, String requestID)
       throws JMSException {
     LOGGER.log(Level.INFO, "handling reservation request ...");
-    // Message responseMessage = null;
+    Message responseMessage = null;
+    Model resultModel = ModelFactory.createDefaultModel();
+    final List<String> sliversList = new LinkedList<>();
+    final Map<String, String> allocateParameters = new HashMap<>();
     
-    Map<String, String> allocateParameters = new HashMap<>();
-    parseReservationParameters(allocateParameters, requestModel);
-    
-    if(isReservationAvailable(allocateParameters.get(componentManagerId))) {
-      // to do: put reservation parameters in repository.
+    this.parseReservationParameters(allocateParameters, requestModel);
+    if (isReservationAvailable(allocateParameters.get(componentManagerId))) {
+      Model reservationModel = this.createReserveModel(allocateParameters, sliversList);
+      this.reserve(reservationModel);
+    } else {
+      // to do: instance can't be reserved.
     }
-    else {
-      // to do: instance can't be reserved. 
+    if(!sliversList.isEmpty()){
+      LOGGER.log(Level.INFO, " resevation is done ");
+      RDFList rdfList = resultModel.createList();
+      for (String createdSlivers : sliversList){ 
+        rdfList.addProperty(resultModel.createProperty(createdSlivers), "sliver");
+      }
     }
-    // to do: send message back to SFA. 
-    // context.createProducer().send(topic, responseMessage);
+    String serializedResponse = MessageUtil.serializeModel(resultModel);
+    responseMessage = MessageUtil.createRDFMessage(serializedResponse, IMessageBus.TYPE_INFORM, serialization,
+        requestID, context);
+    LOGGER.log(Level.INFO, " a reply is sent to SFA ...");
+    context.createProducer().send(topic, responseMessage);
   }
   
-  private void parseReservationParameters(Map<String, String> allocateParameters, Model requestModel){
+  private void parseReservationParameters(Map<String, String> allocateParameters, Model requestModel) {
     StmtIterator iterator = requestModel.listStatements();
     while (iterator.hasNext()) {
       Statement st = iterator.next();
@@ -91,30 +111,81 @@ public class ReservationMDBListener implements MessageListener {
     }
   }
   
-  private boolean isReservationAvailable(String componentManagerId){
-     String query = createSPARQLquery(componentManagerId);
-     try {
-       ResultSet result_set  = QueryExecuter.executeSparqlSelectQuery(query);
-       // to do: parse ResultSet
+  private boolean isReservationAvailable(String componentManagerId) {
+    boolean result = false;
+    String query = createSPARQLquery(componentManagerId);
+    try {
+      ResultSet result_set = QueryExecuter.executeSparqlSelectQuery(query);
+      result = checkRequestResult(result_set);
     } catch (ResourceRepositoryException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-     return true;
+    return result;
   }
   
- private String createSPARQLquery(String componentManagerId){
-   String query = "PREFIX omn: <http://open-multinet.info/ontology/omn#> "
-          + "SELECT ?maxInstances ?reservedInstances ?createdInstances WHERE {"
-          + "OPTIONAL { ?instance a ?resourceType ."
-          + " <" + componentManagerId + "> a ?adapterType ."
-          + "?adapterType omn:implements ?resourceType .}"
-          + "OPTIONAL { <" + componentManagerId + "> omn:maxInstances ?maxInstances  }"
-          + "OPTIONAL { ?reservedInstances omn:reserve_Instance <" + componentManagerId + "> } "
-          + "}";
-   return query;
- }
+  private String createSPARQLquery(String componentManagerId) {
+    String query = "PREFIX omn: <http://open-multinet.info/ontology/omn#> "
+        + "SELECT ?maxInstances ?reservedInstances ?createdInstances WHERE {"
+        + "OPTIONAL { ?instance a ?resourceType ." + " <" + componentManagerId + "> a ?adapterType ."
+        + "?adapterType omn:implements ?resourceType .}" + "OPTIONAL { <" + componentManagerId
+        + "> omn:maxInstances ?maxInstances  }" + "OPTIONAL { ?reservedInstances omn:reserve_Instance_from <"
+        + componentManagerId + "> } " + "}";
+    return query;
+  }
+  
+  private boolean checkRequestResult(ResultSet result) {
+    int maxInstances = 0;
+    int createdInstances = 0;
+    int reservedInstances = 0;
+    while (result.hasNext()) {
+      QuerySolution qs = result.next();
+      if (qs.contains("maxInstances")) {
+        maxInstances = qs.getLiteral("maxInstances").getInt();
+      }
+      if (qs.contains("?createdInstances")) {
+        createdInstances = createdInstances + 1;
+      }
+      if (qs.contains("reservedInstances")) {
+        reservedInstances = reservedInstances + 1;
+      }
+    }
+    if (maxInstances != 0) {
+      if (maxInstances - (createdInstances + reservedInstances) > 0) {
+        return true;
+      } else
+        return false;
+    }
+    return true;
+  }
+  
+  private Model createReserveModel( Map<String, String> allocateParameters, List<String> sliversList){
+    Model reservationModel = ModelFactory.createDefaultModel();
+    String omn = "http://open-multinet.info/ontology/omn#";
+    
+    Resource slice = reservationModel.createResource(allocateParameters.get(URN));
+    slice.addProperty(RDF.type, omn + "slice");
+    if(allocateParameters.containsKey(EndTime)){
+      slice.addProperty(reservationModel.createProperty(omn + EndTime), allocateParameters.get(EndTime));
+    }
+    
+    Resource sliver = reservationModel.createResource(allocateParameters.get(URN) + "+sliver"); // to be changed.
+    sliver.addProperty(RDF.type, omn + "sliver");
+    sliver.addProperty(reservationModel.createProperty(omn + "PartOf"), slice.getURI());
+    sliver.addProperty(reservationModel.createProperty(omn + "reserve_Instance_from"), allocateParameters.get(componentManagerId));
+    sliversList.add(allocateParameters.get(URN) + "+sliver");
+    return reservationModel;
+  }
+  
+  private void reserve(Model model){
+    
+    try {
+      TripletStoreAccessor.updateRepositoryModel(model);
+    } catch (ResourceRepositoryException e) {
+      LOGGER.log(Level.SEVERE, e.getMessage());
+    }
+  }
+  
 
-
-
+  
 }
