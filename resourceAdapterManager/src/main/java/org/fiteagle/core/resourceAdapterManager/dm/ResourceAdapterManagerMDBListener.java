4724d3a7988aef2f2b7b8afd32816051b8264d5c
mitjanikolaus <mitjanikolaus@gmail.com>
2015-01-06 20:05:11 +0100

    made resourceadaptermanager work, deleted outdated "repository" module

--- resourceAdapterManager/src/main/java/org/fiteagle/core/resourceAdapterManager/dm/ResourceAdapterManagerMDBListener.java
+++ resourceAdapterManager/src/main/java/org/fiteagle/core/resourceAdapterManager/dm/ResourceAdapterManagerMDBListener.java
@@ -15,8 +15,12 @@ import javax.jms.Topic;
 import org.fiteagle.api.core.IMessageBus;
 import org.fiteagle.api.core.MessageFilters;
 import org.fiteagle.api.core.MessageUtil;
+import org.fiteagle.api.core.MessageUtil.ParsingException;
+import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor;
+import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 @MessageDriven(name = "ResourceAdapterManagerMDBListener", activationConfig = {
     @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
@@ -40,22 +44,54 @@ public class ResourceAdapterManagerMDBListener implements MessageListener {
       String rdfString = MessageUtil.getRDFResult(message);
       
       if (messageType != null && rdfString != null) {
-        Model messageModel = MessageUtil.parseSerializedModel(rdfString, serialization);
-        
-        //TODO:
-        if (messageType.equals(IMessageBus.TYPE_GET)) {
-        }
         if (messageType.equals(IMessageBus.TYPE_CREATE)) {
+          Model messageModel = MessageUtil.parseSerializedModel(rdfString, serialization);
+          handleCreate(messageModel, serialization, message.getJMSCorrelationID());
+          
+        } else if (messageType.equals(IMessageBus.TYPE_GET)) {
+          handleGet(message, serialization, message.getJMSCorrelationID());
+          
+        } else if (messageType.equals(IMessageBus.TYPE_DELETE)) {
+          Model messageModel = MessageUtil.parseSerializedModel(rdfString, serialization);
+          handleDelete(messageModel);
         }
-        if (messageType.equals(IMessageBus.TYPE_DELETE)) {
-        }
-        
       }
     } catch (JMSException e) {
       LOGGER.log(Level.SEVERE, e.getMessage());
     }
   }
   
- 
+  private void handleGet(Message message, String serialization, String requestID) throws JMSException {
+    Message responseMessage = null;
+    try {
+      String serializedResponse = TripletStoreAccessor.handleSPARQLRequest(MessageUtil.getSPARQLQuery(message), serialization);
+      responseMessage = MessageUtil.createRDFMessage(serializedResponse, IMessageBus.TYPE_INFORM, null, serialization, requestID, context);
+    } catch (ResourceRepositoryException | ParsingException e) {
+      responseMessage = MessageUtil.createErrorMessage(e.getMessage(), requestID, context);
+    } finally {
+      context.createProducer().send(topic, responseMessage);
+    }
+  }
+  
+  private void handleCreate(Model model, String serialization, String requestID) {
+    try {
+      TripletStoreAccessor.updateRepositoryModel(model);
+    } catch (ResourceRepositoryException e) {
+      LOGGER.log(Level.SEVERE, e.getMessage());
+    }
+    Message message = MessageUtil.createRDFMessage(model, IMessageBus.TYPE_INFORM, null, serialization , requestID, context);
+    context.createProducer().send(topic, message);
+  }
+  
+  private void handleDelete(Model model) {
+    try {
+      StmtIterator iter = model.listStatements();
+      while(iter.hasNext()){
+        TripletStoreAccessor.releaseResource(iter.next().getSubject());
+      }      
+    } catch (ResourceRepositoryException e) {
+      LOGGER.log(Level.SEVERE, e.getMessage());
+    }
+  }
   
 }
