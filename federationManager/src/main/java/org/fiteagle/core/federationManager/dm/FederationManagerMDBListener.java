5577e74dbf23711f1665579a8c7f8024f2a5d620
dnehls <daniel.nehls@tu-berlin.de>
2015-07-17 16:07:28 +0200

    fm

--- federationManager/src/main/java/org/fiteagle/core/federationManager/dm/FederationManagerMDBListener.java
+++ federationManager/src/main/java/org/fiteagle/core/federationManager/dm/FederationManagerMDBListener.java
@@ -12,6 +12,7 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.Topic;
 
+import info.openmultinet.ontology.exceptions.InvalidModelException;
 import org.fiteagle.api.core.IMessageBus;
 import org.fiteagle.api.core.MessageFilters;
 import org.fiteagle.api.core.MessageUtil;
@@ -22,53 +23,84 @@ import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor.ResourceRepos
 import com.hp.hpl.jena.rdf.model.Model;
 
 @MessageDriven(name = "FederationManagerMDBListener", activationConfig = {
-    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
-    @ActivationConfigProperty(propertyName = "destination", propertyValue = IMessageBus.TOPIC_CORE),
-    @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = MessageFilters.FILTER_FEDERATION_MANAGER),
-    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge") })
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = IMessageBus.TOPIC_CORE),
+        @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = MessageFilters.FILTER_FEDERATION_MANAGER),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")})
 public class FederationManagerMDBListener implements MessageListener {
-  
-  private static Logger LOGGER = Logger.getLogger(FederationManagerMDBListener.class.toString());
-  
-  @Inject
-  private JMSContext context;
-  @javax.annotation.Resource(mappedName = IMessageBus.TOPIC_CORE_NAME)
-  private Topic topic;
 
-  @Inject
-  FederationManager federationManager;
+    private static Logger LOGGER = Logger.getLogger(FederationManagerMDBListener.class.toString());
 
-  @PostConstruct
-  private void initIfNecessary(){
+    @Inject
+    private JMSContext context;
+    @javax.annotation.Resource(mappedName = IMessageBus.TOPIC_CORE_NAME)
+    private Topic topic;
 
-      federationManager.runSetup();
+    @Inject
+    FederationManager federationManager;
 
-  }
+    @PostConstruct
+    private void initIfNecessary() {
 
-  public void onMessage(final Message message) {
-    String messageType = MessageUtil.getMessageType(message);
-    String serialization = MessageUtil.getMessageSerialization(message);
-    LOGGER.log(Level.INFO, "Received a " + messageType + " message");
-    
-    if (messageType != null) {
-      if (messageType.equals(IMessageBus.TYPE_GET)) {
-        handleGet(serialization, MessageUtil.getJMSCorrelationID(message));
-      }
-    }
-  }
-  
-  private void handleGet(String serialization, String requestID){
-    Model federationModel = null;
-    try {
-    federationModel=  TripletStoreAccessor.getInfrastructure();
-    } catch (ResourceRepositoryException e) {
-      Message message = MessageUtil.createErrorMessage(e.getMessage(), requestID, context);
-      context.createProducer().send(topic, message);
-      return;
+        federationManager.runSetup();
+
     }
 
-    Message message = MessageUtil.createRDFMessage(federationModel, IMessageBus.TYPE_INFORM, null, serialization, requestID, context);
-    context.createProducer().send(topic, message);
-  }
-  
+    public void onMessage(final Message message) {
+        String messageType = MessageUtil.getMessageType(message);
+        String serialization = MessageUtil.getMessageSerialization(message);
+        LOGGER.log(Level.INFO, "Received a " + messageType + " message");
+
+        if (messageType != null) {
+            if (messageType.equals(IMessageBus.TYPE_GET)) {
+                handleGet(serialization, MessageUtil.getJMSCorrelationID(message));
+            }
+            if (messageType.equals(IMessageBus.TYPE_CREATE)) {
+                String rdfString = MessageUtil.getStringBody(message);
+                Model messageModel = MessageUtil.parseSerializedModel(rdfString, serialization);
+                handleCreate(messageModel);
+            }
+            if (messageType.equals(IMessageBus.TYPE_DELETE)) {
+                String rdfString = MessageUtil.getStringBody(message);
+                Model messageModel = MessageUtil.parseSerializedModel(rdfString, serialization);
+                handleDelete(messageModel);
+            }
+        }
+    }
+
+    private void handleDelete(Model messageModel) {
+        try {
+            TripletStoreAccessor.deleteModel(messageModel);
+
+        } catch (ResourceRepositoryException e) {
+            e.printStackTrace();
+        } catch (InvalidModelException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleCreate(Model messageModel) {
+        try {
+            TripletStoreAccessor.addModel(messageModel);
+        } catch (ResourceRepositoryException e) {
+            e.printStackTrace();
+        } catch (InvalidModelException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleGet(String serialization, String requestID) {
+        Model federationModel = null;
+        try {
+            federationModel = TripletStoreAccessor.getInfrastructure();
+        } catch (ResourceRepositoryException e) {
+            Message message = MessageUtil.createErrorMessage(e.getMessage(), requestID, context);
+            context.createProducer().send(topic, message);
+            return;
+        }
+
+        Message message = MessageUtil.createRDFMessage(federationModel, IMessageBus.TYPE_INFORM, null, serialization, requestID, context);
+        context.createProducer().send(topic, message);
+    }
+
 }
