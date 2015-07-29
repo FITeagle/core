5fa5bc8bf6b0bc7f06f7419dce06e2a7722e76a1
alaaalloush <alaa.alloush@gmail.com>
2015-07-29 12:02:39 +0200

    merged resource adapter manager

--- resourceAdapterManager/src/main/java/org/fiteagle/core/resourceAdapterManager/dm/ResourceAdapterManagerMDBListener.java
+++ resourceAdapterManager/src/main/java/org/fiteagle/core/resourceAdapterManager/dm/ResourceAdapterManagerMDBListener.java
@@ -3,6 +3,7 @@ package org.fiteagle.core.resourceAdapterManager.dm;
 import info.openmultinet.ontology.exceptions.InvalidModelException;
 import info.openmultinet.ontology.vocabulary.Omn_lifecycle;
 
+import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,9 +15,11 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.Topic;
 
+import org.apache.jena.atlas.web.HttpException;
 import org.fiteagle.api.core.IMessageBus;
 import org.fiteagle.api.core.MessageFilters;
 import org.fiteagle.api.core.MessageUtil;
+import org.fiteagle.api.core.TimerHelper;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
 
@@ -35,6 +38,8 @@ public class ResourceAdapterManagerMDBListener implements MessageListener {
   
   private static Logger LOGGER = Logger.getLogger(ResourceAdapterManagerMDBListener.class.toString());
   
+  @Inject
+  private TimerHelper helper;
   @Inject
   private JMSContext context;
   @javax.annotation.Resource(mappedName = IMessageBus.TOPIC_CORE_NAME)
@@ -88,11 +93,12 @@ public class ResourceAdapterManagerMDBListener implements MessageListener {
     ResIterator resIterator = model.listSubjectsWithProperty(Omn_lifecycle.canImplement);
     while (resIterator.hasNext()) {
       Resource resource = resIterator.next();
-      try {
-        TripletStoreAccessor.addResource(resource);
-      } catch (ResourceRepositoryException e) {
+/*      try {*/
+    helper.setNewTimer(new ResourceClass(resource));
+//        TripletStoreAccessor.addResource(resource);
+      /*} catch (ResourceRepositoryException e) {
         LOGGER.log(Level.SEVERE, "Could not add " + resource, e);
-      }
+      }*/
     }
     Message message = MessageUtil.createRDFMessage(model, IMessageBus.TYPE_INFORM, null, serialization, requestID, context);
     context.createProducer().send(topic, message);
@@ -101,5 +107,26 @@ public class ResourceAdapterManagerMDBListener implements MessageListener {
   private void handleDelete(Model model) throws InvalidModelException, ResourceRepositoryException {
         TripletStoreAccessor.deleteModel(model);
   }
+  
+  
+	class ResourceClass implements Callable<Void> {
+		Resource resource;
+		
+		public ResourceClass(Resource resource){
+		this.resource = resource;
+		}
+		
+		@Override
+		public Void call() throws ResourceRepositoryException, HttpException {	
+			TripletStoreAccessor.addResource(resource);
+			try {
+				this.finalize();
+			} catch (Throwable e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
+}
   
 }
