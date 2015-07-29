399c4680a6304866dae7af6ed063a20b3b3afd90
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-07-29 14:36:38 +0200

    back to normal

--- resourceAdapterManager/src/main/java/org/fiteagle/core/resourceAdapterManager/dm/ResourceAdapterManagerMDBListener.java
+++ resourceAdapterManager/src/main/java/org/fiteagle/core/resourceAdapterManager/dm/ResourceAdapterManagerMDBListener.java
@@ -3,7 +3,6 @@ package org.fiteagle.core.resourceAdapterManager.dm;
 import info.openmultinet.ontology.exceptions.InvalidModelException;
 import info.openmultinet.ontology.vocabulary.Omn_lifecycle;
 
-import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,19 +14,15 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.Topic;
 
-import org.apache.jena.atlas.web.HttpException;
 import org.fiteagle.api.core.IMessageBus;
 import org.fiteagle.api.core.MessageFilters;
 import org.fiteagle.api.core.MessageUtil;
-import org.fiteagle.api.core.TimerHelper;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 @MessageDriven(name = "ResourceAdapterManagerMDBListener", activationConfig = {
     @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
@@ -38,13 +33,12 @@ public class ResourceAdapterManagerMDBListener implements MessageListener {
   
   private static Logger LOGGER = Logger.getLogger(ResourceAdapterManagerMDBListener.class.toString());
   
-  @Inject
-  private TimerHelper helper;
+
   @Inject
   private JMSContext context;
   @javax.annotation.Resource(mappedName = IMessageBus.TOPIC_CORE_NAME)
   private Topic topic;
-  
+    
   public void onMessage(final Message message) {
     String messageType = MessageUtil.getMessageType(message);
     String serialization = MessageUtil.getMessageSerialization(message);
@@ -92,13 +86,12 @@ public class ResourceAdapterManagerMDBListener implements MessageListener {
   private void handleCreate(Model model, String serialization, String requestID) {
     ResIterator resIterator = model.listSubjectsWithProperty(Omn_lifecycle.canImplement);
     while (resIterator.hasNext()) {
-      Resource resource = resIterator.next();
-/*      try {*/
-    helper.setNewTimer(new ResourceClass(resource));
-//        TripletStoreAccessor.addResource(resource);
-      /*} catch (ResourceRepositoryException e) {
-        LOGGER.log(Level.SEVERE, "Could not add " + resource, e);
-      }*/
+    Resource resource = resIterator.next();
+    try {
+            TripletStoreAccessor.addResource(resource);
+          } catch (ResourceRepositoryException e) {
+            LOGGER.log(Level.SEVERE, "Could not add " + resource, e);
+          }
     }
     Message message = MessageUtil.createRDFMessage(model, IMessageBus.TYPE_INFORM, null, serialization, requestID, context);
     context.createProducer().send(topic, message);
@@ -107,26 +100,5 @@ public class ResourceAdapterManagerMDBListener implements MessageListener {
   private void handleDelete(Model model) throws InvalidModelException, ResourceRepositoryException {
         TripletStoreAccessor.deleteModel(model);
   }
-  
-  
-	class ResourceClass implements Callable<Void> {
-		Resource resource;
-		
-		public ResourceClass(Resource resource){
-		this.resource = resource;
-		}
-		
-		@Override
-		public Void call() throws ResourceRepositoryException, HttpException {	
-			TripletStoreAccessor.addResource(resource);
-			try {
-				this.finalize();
-			} catch (Throwable e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return null;
-		}
-}
   
 }
